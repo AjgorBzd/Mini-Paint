59c3f71f0740096878714787a4e0e634f2b5363e
Igor Bzdawka <83597859+AjgorBzd@users.noreply.github.com>
2023-03-11 19:38:02 +0100

    Add files via upload

---
+++ Mini Paint/Paint2/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿
+namespace Paint2
+{
+    partial class MiniPaint
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MiniPaint));
+            this.Tools = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
+            this.Save = new System.Windows.Forms.ToolStripButton();
+            this.Load = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.Brush = new System.Windows.Forms.ToolStripButton();
+            this.Rectangle = new System.Windows.Forms.ToolStripButton();
+            this.Ellipse = new System.Windows.Forms.ToolStripButton();
+            this.Clear = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripLabel3 = new System.Windows.Forms.ToolStripLabel();
+            this.Thiccness = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripLabel4 = new System.Windows.Forms.ToolStripLabel();
+            this.Chosen_color = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+            this.Language = new System.Windows.Forms.ToolStripLabel();
+            this.English = new System.Windows.Forms.ToolStripButton();
+            this.Polish = new System.Windows.Forms.ToolStripButton();
+            this.Panel = new System.Windows.Forms.TableLayoutPanel();
+            this.Colors = new System.Windows.Forms.GroupBox();
+            this.FlowPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.Canvas = new System.Windows.Forms.PictureBox();
+            this.Tools.SuspendLayout();
+            this.Panel.SuspendLayout();
+            this.Colors.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Canvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Tools
+            // 
+            this.Tools.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.Tools.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel2,
+            this.Save,
+            this.Load,
+            this.toolStripSeparator1,
+            this.toolStripLabel1,
+            this.Brush,
+            this.Rectangle,
+            this.Ellipse,
+            this.Clear,
+            this.toolStripSeparator2,
+            this.toolStripLabel3,
+            this.Thiccness,
+            this.toolStripSeparator3,
+            this.toolStripLabel4,
+            this.Chosen_color,
+            this.toolStripSeparator4,
+            this.Language,
+            this.English,
+            this.Polish});
+            resources.ApplyResources(this.Tools, "Tools");
+            this.Tools.Name = "Tools";
+            this.Tools.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.Tools_ItemClicked);
+            // 
+            // toolStripLabel2
+            // 
+            this.toolStripLabel2.Name = "toolStripLabel2";
+            resources.ApplyResources(this.toolStripLabel2, "toolStripLabel2");
+            // 
+            // Save
+            // 
+            this.Save.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            resources.ApplyResources(this.Save, "Save");
+            this.Save.Name = "Save";
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // Load
+            // 
+            this.Load.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            resources.ApplyResources(this.Load, "Load");
+            this.Load.Name = "Load";
+            this.Load.Click += new System.EventHandler(this.Load_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            resources.ApplyResources(this.toolStripSeparator1, "toolStripSeparator1");
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            resources.ApplyResources(this.toolStripLabel1, "toolStripLabel1");
+            // 
+            // Brush
+            // 
+            this.Brush.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            resources.ApplyResources(this.Brush, "Brush");
+            this.Brush.Name = "Brush";
+            this.Brush.Click += new System.EventHandler(this.Brush_Click);
+            // 
+            // Rectangle
+            // 
+            this.Rectangle.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            resources.ApplyResources(this.Rectangle, "Rectangle");
+            this.Rectangle.Name = "Rectangle";
+            this.Rectangle.Click += new System.EventHandler(this.Rectangle_Click);
+            // 
+            // Ellipse
+            // 
+            this.Ellipse.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            resources.ApplyResources(this.Ellipse, "Ellipse");
+            this.Ellipse.Name = "Ellipse";
+            this.Ellipse.Click += new System.EventHandler(this.Ellipse_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            resources.ApplyResources(this.Clear, "Clear");
+            this.Clear.Name = "Clear";
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            resources.ApplyResources(this.toolStripSeparator2, "toolStripSeparator2");
+            // 
+            // toolStripLabel3
+            // 
+            this.toolStripLabel3.Name = "toolStripLabel3";
+            resources.ApplyResources(this.toolStripLabel3, "toolStripLabel3");
+            // 
+            // Thiccness
+            // 
+            this.Thiccness.Items.AddRange(new object[] {
+            resources.GetString("Thiccness.Items"),
+            resources.GetString("Thiccness.Items1"),
+            resources.GetString("Thiccness.Items2")});
+            this.Thiccness.Name = "Thiccness";
+            resources.ApplyResources(this.Thiccness, "Thiccness");
+            this.Thiccness.SelectedIndexChanged += new System.EventHandler(this.Thiccness_SelectedIndexChanged);
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            resources.ApplyResources(this.toolStripSeparator3, "toolStripSeparator3");
+            // 
+            // toolStripLabel4
+            // 
+            this.toolStripLabel4.Name = "toolStripLabel4";
+            resources.ApplyResources(this.toolStripLabel4, "toolStripLabel4");
+            // 
+            // Chosen_color
+            // 
+            this.Chosen_color.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            resources.ApplyResources(this.Chosen_color, "Chosen_color");
+            this.Chosen_color.Name = "Chosen_color";
+            // 
+            // toolStripSeparator4
+            // 
+            this.toolStripSeparator4.Name = "toolStripSeparator4";
+            resources.ApplyResources(this.toolStripSeparator4, "toolStripSeparator4");
+            // 
+            // Language
+            // 
+            this.Language.Name = "Language";
+            resources.ApplyResources(this.Language, "Language");
+            // 
+            // English
+            // 
+            this.English.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            resources.ApplyResources(this.English, "English");
+            this.English.Name = "English";
+            this.English.Click += new System.EventHandler(this.English_Click);
+            // 
+            // Polish
+            // 
+            this.Polish.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            resources.ApplyResources(this.Polish, "Polish");
+            this.Polish.Name = "Polish";
+            this.Polish.Click += new System.EventHandler(this.Polish_Click);
+            // 
+            // Panel
+            // 
+            resources.ApplyResources(this.Panel, "Panel");
+            this.Panel.Controls.Add(this.Colors, 1, 0);
+            this.Panel.Controls.Add(this.Canvas, 0, 0);
+            this.Panel.Name = "Panel";
+            // 
+            // Colors
+            // 
+            this.Colors.Controls.Add(this.FlowPanel);
+            resources.ApplyResources(this.Colors, "Colors");
+            this.Colors.Name = "Colors";
+            this.Colors.TabStop = false;
+            // 
+            // FlowPanel
+            // 
+            resources.ApplyResources(this.FlowPanel, "FlowPanel");
+            this.FlowPanel.Name = "FlowPanel";
+            // 
+            // Canvas
+            // 
+            this.Canvas.BackColor = System.Drawing.Color.White;
+            this.Canvas.Cursor = System.Windows.Forms.Cursors.Cross;
+            resources.ApplyResources(this.Canvas, "Canvas");
+            this.Canvas.Name = "Canvas";
+            this.Canvas.TabStop = false;
+            this.Canvas.SizeChanged += new System.EventHandler(this.Canvas_SizeChanged);
+            this.Canvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Canvas_MouseDown);
+            this.Canvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Canvas_MouseMove);
+            this.Canvas.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Canvas_MouseUp);
+            // 
+            // MiniPaint
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.Panel);
+            this.Controls.Add(this.Tools);
+            this.Name = "MiniPaint";
+            this.Tools.ResumeLayout(false);
+            this.Tools.PerformLayout();
+            this.Panel.ResumeLayout(false);
+            this.Colors.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Canvas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip Tools;
+        private System.Windows.Forms.TableLayoutPanel Panel;
+        private System.Windows.Forms.GroupBox Colors;
+        private System.Windows.Forms.PictureBox Canvas;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripButton Brush;
+        private System.Windows.Forms.FlowLayoutPanel FlowPanel;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel2;
+        private System.Windows.Forms.ToolStripButton Save;
+        private System.Windows.Forms.ToolStripButton Load;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton Rectangle;
+        private System.Windows.Forms.ToolStripButton Ellipse;
+        private System.Windows.Forms.ToolStripButton Clear;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel3;
+        private System.Windows.Forms.ToolStripComboBox Thiccness;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel4;
+        private System.Windows.Forms.ToolStripButton Chosen_color;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator4;
+        private System.Windows.Forms.ToolStripLabel Language;
+        private System.Windows.Forms.ToolStripButton English;
+        private System.Windows.Forms.ToolStripButton Polish;
+    }
+}
+
---
+++ Mini Paint/Paint2/Form1.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+using System.Threading;
+
+namespace Paint2
+{
+    public partial class MiniPaint : Form
+    {
+        public ComponentResourceManager langhandler;
+        bool brush = false;
+        private Bitmap drawArea;
+        int x,y;
+        bool move = false;
+        private Graphics g;
+        bool rect = false, elip = false;
+        Point draw;
+        bool langg = false; // false = english, true = polish
+        
+        Pen p;
+        public MiniPaint()
+        {
+            this.langhandler = new ComponentResourceManager(this.GetType());
+            InitializeComponent();
+            Chosen_color.BackColor = Color.Black;
+            drawArea = new Bitmap(Canvas.Size.Width, Canvas.Size.Height);
+            Canvas.Image = drawArea;
+            g = Canvas.CreateGraphics();
+            CultureInfo.CurrentUICulture = new CultureInfo("en");
+            
+            //g.Clear(Color.Red);
+            p = new Pen(Color.Black, 2);
+           foreach (KnownColor color in Enum.GetValues(typeof(KnownColor)))
+            {
+                Button bb = new Button();
+                bb.BackColor = Color.FromKnownColor(color);
+                bb.Size = new(25, 25);
+                bb.Click += new EventHandler(button_Click);
+                bb.Name = color.ToString();
+                FlowPanel.Controls.Add(bb);
+            }
+            FlowPanel.AutoScroll = true;
+            FlowPanel.WrapContents = false;
+            English.Checked = true;   
+            Thiccness.SelectedIndex = 1;
+            
+        }
+        private void button_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            p.Color = Color.FromName(button.Name);
+            Chosen_color.BackColor = p.Color;
+
+        }
+        
+
+
+        private void Canvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            move= true;
+            x = e.X;
+            y = e.Y;
+            draw = e.Location;
+
+        }
+
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                if (move && brush)
+                {
+                    using (Graphics pplz = Graphics.FromImage(drawArea))
+                    {
+                        pplz.DrawLine(p, new Point(x, y), e.Location);
+                    }
+                    x = e.X;
+                    y = e.Y;
+                    //Canvas.Refresh();
+                }
+                if(move&&rect)
+                {
+                    Bitmap tmp = new Bitmap(drawArea, Canvas.Size.Width, Canvas.Size.Height);
+                    Canvas.Image = tmp;
+                    using (Graphics pplz = Graphics.FromImage(tmp))
+                        pplz.DrawRectangle(p, Math.Min(e.X, x), Math.Min(e.Y, y), Math.Abs(e.X - x), Math.Abs(e.Y - y));
+                }
+                if(move&&elip)
+                {
+                    Bitmap tmp = new Bitmap(drawArea, Canvas.Size.Width, Canvas.Size.Height);
+                    Canvas.Image = tmp;
+                    using (Graphics pplz = Graphics.FromImage(tmp))
+                        pplz.DrawEllipse(p, Math.Min(e.X, x), Math.Min(e.Y, y), Math.Abs(e.X - x), Math.Abs(e.Y - y));
+                }
+                Canvas.Refresh();
+            }
+            if(e.Button==MouseButtons.Right)
+            {
+                move = false;
+            }
+            //Canvas.Invalidate();
+        }
+
+        private void Canvas_MouseUp(object sender, MouseEventArgs e)
+        {
+            Canvas.Image = drawArea;
+           if(e.Button==MouseButtons.Left)
+            {
+
+               if(move)
+                {
+                    if (rect)
+                    {
+                        using (Graphics rys = Graphics.FromImage(drawArea))
+                        {
+                            rys.DrawRectangle(p, Math.Min(e.X, x), Math.Min(e.Y, y), Math.Abs(e.X - draw.X), Math.Abs(e.Y - draw.Y));
+                        }
+                        Canvas.Refresh();
+                    }
+                    else if (elip)
+                    {
+                        using (Graphics rys = Graphics.FromImage(drawArea))
+                            rys.DrawEllipse(p, x, y, e.X - x, e.Y - y);
+                    }
+                }
+                Canvas.Refresh();
+                move = false;
+            }
+           if(e.Button==MouseButtons.Right)
+            {
+                move = false;
+            }
+        }
+
+        private void Canvas_SizeChanged(object sender, EventArgs e)
+        {
+            Bitmap tmp = new Bitmap(Canvas.Size.Width, Canvas.Size.Height);
+            using (Graphics g2 = Graphics.FromImage(tmp))
+            {
+                g2.DrawImage(drawArea, new Point(0, 0));
+            }
+            drawArea = tmp;
+            Canvas.Image = drawArea;
+            Canvas.Refresh();
+
+
+
+            //Canvas.Refresh();
+            //Canvas.Invalidate();
+            //Canvas.Image = tmp;
+            //label1.Text = String.Format("{0} {1}", Canvas.Size.Width, Canvas.Size.Height);
+        }
+
+        private void Tools_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void Rectangle_Click(object sender, EventArgs e)
+        {
+            if (rect) 
+            {
+                rect = false;
+                Rectangle.Checked = false;
+            }
+            else
+            {
+                Ellipse.Checked = false;
+                Brush.Checked = false;
+                Rectangle.Checked = true;
+                rect = true;
+                brush = false;
+                elip = false;
+            }
+        }
+
+        private void Ellipse_Click(object sender, EventArgs e)
+        {
+            if (elip)
+            {
+                elip = false;
+                Ellipse.Checked = false;
+            }
+
+            else
+            {
+                Ellipse.Checked = true;
+                Brush.Checked = false;
+                Rectangle.Checked = false;
+                elip = true;
+                brush = false;
+                rect = false;
+            }
+        }
+
+        private void Thiccness_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ToolStripComboBox comboBox = (ToolStripComboBox)sender;
+            int radius = Convert.ToInt32(comboBox.SelectedItem);
+            p.Width = radius;
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog save = new SaveFileDialog();
+            save.Title = "Save Image...";
+            save.Filter = "bmp files (*.bmp)|*.bmp; jpeg files (*.jpeg)|*.jpeg; png files (*.png)|*.png";
+            if (save.ShowDialog() == DialogResult.OK)
+            {
+                drawArea.Save(save.FileName);
+            }
+        }
+
+        private void Load_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog load = new OpenFileDialog())
+            {
+                load.Title = "Open Image...";
+                load.Filter = "bmp files (*.bmp)|*.bmp; jpeg files (*.jpeg)|*.jpeg; png files (*.png)|*.png";
+
+                if (load.ShowDialog() == DialogResult.OK)
+                {
+                    drawArea = new Bitmap(load.FileName);
+                    int width_difference = drawArea.Width - Canvas.Width;
+                    int height_difference = drawArea.Height - Canvas.Height;
+                    Size = new Size(Size.Width + width_difference, Size.Height + height_difference);
+                    Canvas.Image = drawArea;
+                }
+            }
+        }
+
+        private void English_Click(object sender, EventArgs e)
+        {
+            English.Checked = true;
+            Polish.Checked = false;
+            
+        }
+
+        private void Polish_Click(object sender, EventArgs e)
+        {
+            English.Checked = false;
+            Polish.Checked = true;
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            using (Graphics pplz = Graphics.FromImage(drawArea))
+                pplz.Clear(Color.White);
+            Canvas.Refresh();
+        }
+
+        private void Brush_Click(object sender, EventArgs e)
+        {
+            if (brush)
+            {
+                brush = false;
+                Brush.Checked = false;
+            }
+            else
+            {
+                Brush.Checked = true;
+                Rectangle.Checked = false;
+                Ellipse.Checked = false;
+                brush = true;
+                rect = false;
+                elip = false;
+            }
+            
+        }
+    }
+}
